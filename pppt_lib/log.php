1c39c3e704a66e7eb959955dba0a2220423ea298
mkgask <mkgask@zsw.jp>
2016-03-27 19:18:30 +0900

    Can be added prefix and postfix in log file name.

--- pppt_lib/log.php
+++ pppt_lib/log.php
@@ -3,11 +3,14 @@
 class log {
 
     static $log_path = '';
+    static $prefix   = '';
+    static $postfix  = '';
 
     static function w($write, $file = '', $line = '', $class = '', $function = '') {
         if($file && $line && $class && $function) {
             $write = "$file:$line : $class::$function : $write";
         }
+        $write = str_replace(["\r", "\n"], ["[cr]\r", "[lf]\n"], $write);
         list($usec, $sec) = explode(" ", microtime());
         $usec = substr($usec, 1);
         $ymdhis = date('Y/m/d H:i:s', $sec);
@@ -18,13 +21,33 @@ class log {
         );
     }
 
+    static function prefix($prefix = '') {
+        if(!$prefix) { return self::$prefix; }
+        return self::$prefix = $prefix;
+    }
+
+    static function postfix($postfix = '') {
+        if(!$postfix) { return self::$postfix; }
+        return self::$postfix = $postfix;
+    }
+
     static function path() {
         if(self::$log_path) {
             return self::$log_path;
         }
         $log_dir = __DIR__.'/log/';
         if(!is_dir($log_dir)) { mkdir(__DIR__.'/log/'); }
-        self::$log_path = $log_dir. date('Ymd').'.log';
+        if(self::$prefix && self::$postfix) {
+            self::$log_path = $log_dir. self::$prefix. '-'. date('Ymd'). '-'. self::$postfix. '.log';
+        } else
+        if(self::$prefix) {
+            self::$log_path = $log_dir. self::$prefix. '-'. date('Ymd').'.log';
+        } else
+        if(self::$postfix) {
+            self::$log_path = $log_dir. date('Ymd'). '-'. self::$postfix. '.log';
+        } else {
+            self::$log_path = $log_dir. date('Ymd').'.log';
+        }
         return self::$log_path;
     }
 
